53e661d0b67070262f664634d93d192e6ff2d731
saggeseniccolo <snicco2004@gmail.com>
2024-04-09 14:48:04 +0200

    Scaffold Brand Controller and Views

--- NotebookStoreMVC/Controllers/BrandController.cs
+++ NotebookStoreMVC/Controllers/BrandController.cs
@@ -1,58 +1,163 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using NotebookStore.Entities;
+using NotebookStoreContext;
 
-namespace NotebookStoreMVC.Controllers;
-
-public class BrandController : Controller
+namespace NotebookStoreMVC.Controllers
 {
-    // GET: /<controller>/
-    public IActionResult Index()
+    public class BrandController : Controller
     {
-        var context = new NotebookStoreContext.NotebookStoreContext();
+        private readonly NotebookStoreContext.NotebookStoreContext _context;
 
-        var brandModel = context.Brands.ToList();
-
-        ViewData["Brands"] = brandModel;
-
-        return View();
-    }
-    // GET: Brand/Delete/
-    public async Task<IActionResult> Delete(int? Id)
-    {
-        var context = new NotebookStoreContext.NotebookStoreContext();
-
-        if (Id == null)
+        public BrandController(NotebookStoreContext.NotebookStoreContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        var brand = await context.Brands
-            .FirstOrDefaultAsync(b => b.Id == Id);
-        if (brand == null)
+        // GET: Brand
+        public async Task<IActionResult> Index()
         {
-            return NotFound();
+            return _context.Brands != null ?
+                        View(await _context.Brands.ToListAsync()) :
+                        Problem("Entity set 'NotebookStoreContext.Brands'  is null.");
         }
 
-        return View(brand);
-    }
-
-    // POST: Movies/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        var context = new NotebookStoreContext.NotebookStoreContext();
-        var brand = await context.Brands.FindAsync(id);
-        if (brand != null)
+        // GET: Brand/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            context.Brands.Remove(brand);
+            if (id == null || _context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
         }
 
-        await context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
+        // GET: Brand/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Brand/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(brand);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Brand/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: Brand/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Brand brand)
+        {
+            if (id != brand.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(brand);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BrandExists(brand.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        // GET: Brand/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _context.Brands
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View(brand);
+        }
+
+        // POST: Brand/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Brands == null)
+            {
+                return Problem("Entity set 'NotebookStoreContext.Brands'  is null.");
+            }
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand != null)
+            {
+                _context.Brands.Remove(brand);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BrandExists(int id)
+        {
+            return (_context.Brands?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
-
-
-
-    
 }
