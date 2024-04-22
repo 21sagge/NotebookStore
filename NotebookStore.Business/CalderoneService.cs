ae9f5531a526fd57c3dffb163ea2ee8635fb0d24
saggeseniccolo <snicco2004@gmail.com>
2024-04-22 15:56:53 +0200

    Add CalderoneService and BrandDto classes

---
+++ NotebookStore.Business/CalderoneService.cs
@@ -0,0 +1,14 @@
+﻿namespace NotebookStore.Business;
+
+using NotebookStore.DAL;
+
+public class CalderoneService
+{
+	private readonly IUnitOfWork unitOfWork;
+
+	public CalderoneService(IUnitOfWork unitOfWork)
+	{
+		this.unitOfWork = unitOfWork;
+	}
+
+}
