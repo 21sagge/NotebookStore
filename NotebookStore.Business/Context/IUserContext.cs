81959edce3c1de28446d2a98fb83803e5a9d582a
Niccolò Saggese <snicco2004@gmail.com>
2024-07-22 09:29:27 +0200

    Add GetCurrentUser method to IUserContext interface

--- NotebookStore.Business/Context/IUserContext.cs
+++ NotebookStore.Business/Context/IUserContext.cs
@@ -4,6 +4,10 @@ namespace NotebookStore.Business.Context
 {
     public interface IUserContext
     {
+        /// <summary>
+        /// Get the current user
+        /// </summary>
+        /// <returns>ClaimsPrincipal</returns>
         public ClaimsPrincipal? GetCurrentUser();
     }
 }
